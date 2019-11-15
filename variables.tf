556e1d3781eee4c12eac472b0c13ed8b60a11b4d
Fabriyerovi <fabriyerovi@CrossFit-MBPro.local>
2019-11-15 14:11:21 -0500

    First commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The Prefix used for all resources in this example"
+  default = "sbatf"
+}
+
+variable "location" {
+  description = "The Azure Region in which the resources in this example should exist"
+  default = "eastus"
+}
