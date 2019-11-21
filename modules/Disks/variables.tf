c855c0f4b5caac58e753558bb42c4fc170b48242
Anudeep <anjdee@Anudeeps-MacBook-Pro.local>
2019-11-21 15:55:12 -0500

    disks variables fix

--- modules/Disks/variables.tf
+++ modules/Disks/variables.tf
@@ -1,21 +1,17 @@
 variable prefix{
     description = "This is a prefix added to new resource being added"
-    default = "SBAtf"
 }
 
 variable location{
     description = "This is a prefix added to new resource being added"
-    default = "East US"
 }
 
 variable rg{
     description = "This is a prefix added to new resource being added"
-    default = "SBAtf"
 }
 
 variable virtual_machine_id{
     description = "This is a prefix added to new resource being added"
-    default = "SBAtf"
 }
 
 
