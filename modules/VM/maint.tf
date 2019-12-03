a18a92796a4006c107ed5c7d9064f30440753cc8
Anudeep <anjdee@Anudeeps-MacBook-Pro.local>
2019-12-03 10:04:10 -0500

    Added domain join

--- modules/VM/maint.tf
+++ modules/VM/maint.tf
@@ -1,15 +1,3 @@
-resource "azurerm_network_interface" "vm" {
-  name                = "${var.prefix}-nic"
-  location            = var.location
-  resource_group_name = var.rg
-
-  ip_configuration {
-    name                          = "configuration"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 # Create a Linux virtual machine
 resource "azurerm_virtual_machine" "vm" {
   name                  = var.prefix
@@ -45,4 +33,44 @@ resource "azurerm_virtual_machine" "vm" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+}
+
+resource "azurerm_network_interface" "vm" {
+  name                = "${var.prefix}-nic"
+  location            = var.location
+  resource_group_name = var.rg
+
+  ip_configuration {
+    name                          = "configuration"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "MYADJOINEDVMADDE" {
+  name                 = "MYADJOINEDVMADDE"
+  location             = var.location
+  resource_group_name  = var.rg
+  virtual_machine_name = azurerm_virtual_machine.vm.name
+  publisher            = "Microsoft.Compute"
+  type                 = "JsonADDomainExtension"
+  type_handler_version = "1.3"
+
+  # What the settings mean: https://docs.microsoft.com/en-us/windows/desktop/api/lmjoin/nf-lmjoin-netjoindomain
+
+  settings = <<SETTINGS
+    {
+        "Name": "SBATEST.COM",
+        "OUPath": "OU=servers,DC=sbatest,DC=com",
+        "User": "sbatest.com\\sbatest",
+        "Restart": "true",
+        "Options": "3"
+    }
+SETTINGS
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+      "Password": "$BAtesting2019"
+    }
+  PROTECTED_SETTINGS
+  depends_on = ["azurerm_virtual_machine.vm"]
 }
