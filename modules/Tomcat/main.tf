94c08a5f2156704a8bb128f9ca5ff1b1acef45e6
Anudeep <anjdee@Anudeeps-MacBook-Pro.local>
2019-12-16 14:28:30 -0500

    direct code test bug fix

--- modules/Tomcat/main.tf
+++ modules/Tomcat/main.tf
@@ -30,10 +30,10 @@ resource "null_resource" "provision_web" {
     }
 
     inline = [
-      "$URL=(Invoke-WebRequest -UseBasicParsing https://www.java.com/en/download/manual.jsp).Content | %{[regex]::matches($_, '(?:<a title="Download Java software for Windows Online" href=")(.*)(?:">)').Groups[1].Value}
-        Invoke-WebRequest -UseBasicParsing -OutFile jre8.exe $URL
-        Start-Process .\jre8.exe '/s REBOOT=0 SPONSORS=0 AUTO_UPDATE=0' -wait
-        echo $?"
+      "$URL=(Invoke-WebRequest -UseBasicParsing https://www.java.com/en/download/manual.jsp).Content | %{[regex]::matches($_, '(?:<a title="Download Java software for Windows Online" href=")(.*)(?:">)').Groups[1].Value}",
+        "Invoke-WebRequest -UseBasicParsing -OutFile jre8.exe $URL",
+        "Start-Process .\jre8.exe '/s REBOOT=0 SPONSORS=0 AUTO_UPDATE=0' -wait",
+        "echo $?"
       
     ]
   }
